16d0d26889325e4a914a6af6aee119eb01d80d4f
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-10-07 22:00:00 +0100

    allow compilation db handling for interface target

--- cmake/modules/Util.cmake
+++ cmake/modules/Util.cmake
@@ -77,11 +77,22 @@ function(attach_compilation_db)
 
   set(file "compile_commands.json")
 
+  get_target_property(TRGT_TYPE ${acdb_TARGET} TYPE)
+  file(TO_CMAKE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/${file}" GENERATED_FILE)
+
+  if(${TRGT_TYPE} STREQUAL "INTERFACE_LIBRARY")
+    add_custom_command(OUTPUT ${GENERATED_FILE}
+      COMMAND ${CMAKE_COMMAND}
+      ARGS -E copy_if_different ${file} ${GENERATED_FILE}
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      VERBATIM)
+  else()
   add_custom_command(TARGET ${acdb_TARGET} POST_BUILD
     COMMAND ${CMAKE_COMMAND}
-    ARGS -E copy_if_different ${file} "${CMAKE_CURRENT_SOURCE_DIR}/${file}"
+        ARGS -E copy_if_different ${file} ${GENERATED_FILE}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     VERBATIM)
+  endif()
 endfunction()
 
 
@@ -108,7 +119,7 @@ function(set_policies)
         message(STATUS "policy ${plc}: ${newval}")
       endif()
     else()
-      message(STATUS "policy ${plc} is not defined")
+      message(WARNING "policy ${plc} is not defined")
     endif()
   endforeach()
 endfunction()
