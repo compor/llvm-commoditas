87ea90f7868dac488009597d8704b1c9f5c1ee84
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2019-04-04 21:00:51 +0100

    Update for latest LLVM version

--- lib/FunctionUtils.cpp
+++ lib/FunctionUtils.cpp
@@ -7,6 +7,9 @@
 #include "llvm/IR/Function.h"
 // using llvm::Function
 
+#include "llvm/IR/Module.h"
+// using llvm::Module
+
 #include "llvm/IR/Instructions.h"
 // using llvm::AllocaInst
 // using llvm::LoadInst
@@ -15,11 +18,13 @@
 namespace icsa {
 
 void DemoteFunctionArgs(llvm::Function &F) {
+  auto as = F.getParent()->getDataLayout().getAllocaAddrSpace();
+
   for (auto &Arg : F.args()) {
     auto *FirstI = F.begin()->getFirstNonPHIOrDbgOrLifetime();
 
     auto *ArgTy = Arg.getType();
-    auto *AI = new llvm::AllocaInst(ArgTy, "farg");
+    auto *AI = new llvm::AllocaInst(ArgTy, as, "farg");
     auto *LI = new llvm::LoadInst(AI, "load_farg");
     auto *SI = new llvm::StoreInst(&Arg, AI);
 
@@ -35,4 +40,4 @@ void DemoteFunctionArgs(llvm::Function &F) {
   return;
 }
 
-} // namespace icsa end
+} // namespace icsa
